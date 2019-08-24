75ab126a889a8b9ac34e0e3ab0bc9f3cc3084ded
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 11:30:58 +0200

    remove unused var NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
