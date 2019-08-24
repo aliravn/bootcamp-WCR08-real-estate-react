e7699decd0fdb7c4663e40057189f76b4b0e5be6
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 11:56:36 +0200

    finish  style Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -7,13 +7,13 @@ class Searchbar extends Component {
 		
 		return (
 			<div id="searchbar">
-				<p className="searchbar-header">What are you looking for?</p>
 				<select>
 					<option value="flat">Flat</option>
 					<option value="house">House</option>
 					<option value="villa">Villa</option>
 					<option value="castle">Castle</option>
 				</select>
+				<p className="searchbar-header">What are you looking for?</p>
 				<input type="text" placeholder="City"/>
 				<input type="text" placeholder="Price up to" />
 				<button id="search-button">Search</button>
