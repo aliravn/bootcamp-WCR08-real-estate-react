5f4136c4b54c12b4da324b9050f1cc2f0e14299e
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 16:11:35 +0200

    separate QuAn into a component and implement it

--- src/components/BuyPageGuide.js
+++ src/components/BuyPageGuide.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import QuAn from "./BuyPageQuAn";
 import "./BuyPageGuide.css"
 
 class BuyPageGuide extends Component {
@@ -9,26 +10,10 @@ class BuyPageGuide extends Component {
 			<div className="buyer-guide-container">
 				<h2>Buyer Guides</h2>
 				<h3>Everything you need to know when looking at different types of homes for sale all in one place.</h3>
-				<div className="qa-container">
-					<p><span>Question:</span> Placeholder text lorem ipsum dolor sit amet?</p>
-					<p><span>Answer:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-				</div>
-				<div className="qa-container">
-					<p><span>Question:</span> Placeholder text lorem ipsum dolor sit amet?</p>
-					<p><span>Answer:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-				</div>
-				<div className="qa-container">
-					<p><span>Question:</span> Placeholder text lorem ipsum dolor sit amet?</p>
-					<p><span>Answer:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-				</div>
-				<div className="qa-container">
-					<p><span>Question:</span> Placeholder text lorem ipsum dolor sit amet?</p>
-					<p><span>Answer:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-				</div>												
+				<QuAn />
+				<QuAn />
+				<QuAn />
+				<QuAn />											
 			</div>
 		);
 	} 
