24563327b91461f9e853dda4cd9cf1fd39e0b536
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 15:47:01 +0200

    update image paths

--- src/components/AboutTeamInfo.js
+++ src/components/AboutTeamInfo.js
@@ -7,22 +7,22 @@ class AboutTeamInfo extends Component {
 			<div className="team-pic-container">
 				<h2>Our Team</h2>
 				<div className="team-foto">
-					<img src={require("../img/team_caitlin.png")} />
+					<img src={require("../img/team/team_caitlin.png")} />
 				</div>
 				<div className="team-foto">
-					<img src={require("../img/team_jen.png")} />
+					<img src={require("../img/team/team_jen.png")} />
 				</div>
 				<div className="team-foto">
-					<img src={require("../img/team_jude.png")} />
+					<img src={require("../img/team/team_jude.png")} />
 				</div>
 				<div className="team-foto">
-					<img src={require("../img/team_nikki.png")} />
+					<img src={require("../img/team/team_nikki.png")} />
 				</div>
 				<div className="team-foto">
-					<img src={require("../img/team_wyatt.png")} />
+					<img src={require("../img/team/team_wyatt.png")} />
 				</div>
 				<div className="team-foto">
-					<img src={require("../img/team-jonesy.png")} />
+					<img src={require("../img/team/team-jonesy.png")} />
 				</div>					
 			</div>
 		);
