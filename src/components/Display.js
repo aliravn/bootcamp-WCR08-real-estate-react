4edd2635994e0bcfc01b3cf51b3aca1772a14013
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 12:18:32 +0200

    add more articles in display

--- src/components/Display.js
+++ src/components/Display.js
@@ -10,9 +10,15 @@ class Display extends Component {
 		return (
 			<div id="content-container">
 				<div id="article-container">
+					<Article image={require("../img/house1.jpg")} />
+					<Article image={require("../img/house2.jpg")} />
+					<Article image={require("../img/house3.jpg")} />
 					<Article image={require("../img/flat1.jpg")} />
 					<Article image={require("../img/flat2.jpg")} />
-					<Article image={require("../img/flat3.jpg")} />														
+					<Article image={require("../img/flat3.jpg")} />
+					<Article image={require("../img/villa1.jpg")} />
+					<Article image={require("../img/villa2.jpg")} />
+					<Article image={require("../img/villa3.jpg")} />												
 				</div>
 				<ButtonViewMore />
 			</div>
