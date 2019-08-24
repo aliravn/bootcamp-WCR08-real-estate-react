4d33a64909459f3e535435af2c43bcf60a399b40
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 11:12:21 +0200

    rename ExtraPage4 into SellPage

---
+++ src/components/SellPage.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import Header from './Header';
+import "./SellPage.css"
+
+class SellPage extends Component {
+
+	render() {
+		
+		return (
+			<div id="sell-page-container">
+				<Header image={require("../img/header_forsale.jpg")} />
+				<div className="sell-page-content">
+				</div>
+			</div>
+		);
+	} 
+}
+
+export default SellPage;
