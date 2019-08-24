504807e298c3c9d9ef367137f6906f3f59d88d9b
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 09:17:25 +0200

    add input form to login page

--- src/components/ExtraPage3.js
+++ src/components/ExtraPage3.js
@@ -11,7 +11,10 @@ class LoginPage extends Component {
 				<Header image={require("../img/header_flat.jpg")} />
 				<div className="login-page-content">
 					<div id="login-container">
-
+						<form>
+							<span>Login: <input type="text" maxlength="20" placeholder="login" /></span>
+							<span>Password:<input type="password" maxlength="20" placeholder="password" /></span>
+						</form>
 					</div>
 				</div>
 			</div>
