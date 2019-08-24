c347cc6c8e2755ac1345edbc7c920d97798e40a9
Valeria Nazarova <alira.vn@gmail.com>
2019-08-24 14:55:40 +0200

    add Conponent to About Page, needs to b separated

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,13 +2,37 @@ import React, {Component} from "react";
 import Header from "./Header";
 import Story from "./Story";
 import Timeline from './Timeline';
+import "./AboutTeamInfo.css"
 
 class AboutPage extends Component {
 	render() {
 		return (
 			<div id="content-container">
-				<Header image={require("../img/header_sale.jpg")} />
+				<Header image={require("../img/header_castle.png")} />
 				<Story />
+
+				<div className="team-pic-container">
+					<h2>Our Team</h2>
+					<div className="team-foto">
+						<img src={require("../img/team_caitlin.png")} />
+					</div>
+					<div className="team-foto">
+						<img src={require("../img/team_jen.png")} />
+					</div>
+					<div className="team-foto">
+						<img src={require("../img/team_jude.png")} />
+					</div>
+					<div className="team-foto">
+						<img src={require("../img/team_nikki.png")} />
+					</div>
+					<div className="team-foto">
+						<img src={require("../img/team_wyatt.png")} />
+					</div>
+					<div className="team-foto">
+						<img src={require("../img/team-jonesy.png")} />
+					</div>					
+				</div>
+
 				<Timeline />
 			</div>
 		);
